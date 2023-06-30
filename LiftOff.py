9aa1aed5d39418371afc46397a2718576aa662bd
C0DELLION <138213261+C0DELLION@users.noreply.github.com>
2023-06-30 19:24:08 +0700

    LiftOff.py

---
+++ LiftOff.py
@@ -0,0 +1,16 @@
+"""
+Program: Liftoff
+--------------------
+Write a program that prints out the calls for a spaceship that is about to launch. Countdown from 10 to 1 and then output Liftoff!
+"""
+
+def main():
+    for i in range(1,11)[::-1]:
+        print(i) 
+    print("Liftoff!")
+
+
+# This provided line is required at the end of
+# Python file to call the main() function.
+if __name__ == '__main__':
+    main()
